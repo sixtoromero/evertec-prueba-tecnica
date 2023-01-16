faf6a372547698555a3ac96f0da8db699fc578ec
Sixto Jose Romero Martinez <sixto.jose@gmail.com>
2023-01-16 04:25:39 -0500

    Se aplican cambios para actualizar info

--- evertec.Services.WebAPIRest/Controllers/ClientesController.cs
+++ evertec.Services.WebAPIRest/Controllers/ClientesController.cs
@@ -81,8 +81,7 @@ namespace evertec.Services.WebAPIRest.Controllers
             }
         }
 
-        [HttpPost("InsertAsync")]
-        //public async Task<IActionResult> InsertAsync(IFormFile File, [FromForm] ClienteDTO modelDto)
+        [HttpPost("InsertAsync")]        
         public async Task<IActionResult> InsertAsync(IFormFile File, [FromForm] string jsonModel)
         {
             ClienteDTO modelDto = Newtonsoft.Json.JsonConvert.DeserializeObject<ClienteDTO>(jsonModel);
@@ -150,12 +149,42 @@ namespace evertec.Services.WebAPIRest.Controllers
         }
 
         [HttpPut("UpdateAsync")]
-        public async Task<IActionResult> UpdateAsync([FromBody] ClienteDTO modelDto)
+        public async Task<IActionResult> UpdateAsync(IFormFile File, [FromForm] string jsonModel)
         {
+            ClienteDTO modelDto = Newtonsoft.Json.JsonConvert.DeserializeObject<ClienteDTO>(jsonModel);
             Response<bool> response = new Response<bool>();
+            string fileExt = string.Empty;
+            string hosturl = "https://localhost:7294/Resources/images/";
+            string fileName = Guid.NewGuid().ToString();
+            
+            var fileResp = modelDto.Foto.Split('/');
 
             try
             {
+                if (File != null)
+                {
+                    if (File.Length > 0)
+                    {
+                        fileExt = Path.GetExtension(File.FileName);
+                        string pathDirectory = Path.Combine(env.WebRootPath, $"Resources\\images\\");
+                        if (!Directory.Exists(pathDirectory))
+                        {
+                            Directory.CreateDirectory(pathDirectory);
+                        }
+
+                        var fileExists = Path.Combine(pathDirectory, fileName);
+                        FileInfo fi = new FileInfo(fileExists);
+                        if (fi.Exists)
+                        {
+                            System.IO.File.Delete(fileExists);
+                            fi.Delete();
+                        }
+
+                        var uploading = Path.Combine(pathDirectory, fileName + fileExt);
+                        modelDto!.Foto = Path.Combine(hosturl, fileName + fileExt);
+
+                    }
+                }
                 if (modelDto == null)
                     return BadRequest();
 
